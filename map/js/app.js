b97ea5026da27cb9a7102a03071f75387b79c2eb
Haskman <sahinmehmetutku@gmail.com>
2016-02-14 11:35:18 -0500

    Map changes commit
    Expandable lists, bug fixes, code quality improvements

--- map/js/app.js
+++ map/js/app.js
@@ -1,11 +1,14 @@
-//Place data
+//Global variables
+var map;
+placeMarkers = ko.observableArray([]);
 
-var places = [{
-        "title": "Cookout",
-        "description": "Cheapest fast food place in the area. The best choice when the fact of being broke becomes more urgent than eating well",
-        "coordinates": {lat: 34.034523, lng: -84.571618},
-        "type": "Food"
-    },
+//Place data
+places = [{
+    "title": "Cookout",
+    "description": "Cheapest fast food place in the area. The best choice when the fact of being broke becomes more urgent than eating well",
+    "coordinates": {lat: 34.034523, lng: -84.571618},
+    "type": "Food"
+},
 
     {
         "title": "Cobb Center Mall",
@@ -36,11 +39,6 @@ var places = [{
     },
 ];
 
-//Global variables
-map;
-placeMarkers = ko.observableArray([]);
-placeWikis = ko.observableArray([]);
-
 
 //Initialize the map
 function initMap() {
@@ -61,39 +59,11 @@ function initMap() {
             return "<h3 class = 'view-text'>" + this.title + "</h4>";
         }
 
-        function wikiInit(callback){
-            this.wikiMarkup = "";
-
-            var wikiTitle = places[i].title.replace(" ", "_");
-            var wikiURL = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles="+this.title;
-
-            $.ajax({
-                type:"GET",
-                dataType:"jsonp",
-                async: false,
-                url: wikiURL,
-
-                success: function(result){
-                    callback(result.query.pages[Object.keys(result.query.pages)[0]].extract);
-                },
-                error: function(){
-
-                }
-            });
-        };
-
-        wikiInit(function(wikiString){
-            var wikiMarkup = "<h4>Wikipedia:</h4>" + "<p>" + wikiString + "</p>";
-
-            marker.infoWindow.content += wikiMarkup;
-        });
-
         function infoWindowContent(place){
             var contentString = "<h4>"+this.type+"</h4>"+
                 "<p class = 'view-text'>"+this.desc+"</p>"+
-                "<img class = 'view-img' src='https://maps.googleapis.com/maps/api/streetview?size=400x400&location="
-                +this.cords.lat+","+this.cords.lng+"'>"+
-                this.wikiContent;
+                "<img class = 'view-img' src='https://maps.googleapis.com/maps/api/streetview?size=400x400&location=" +
+                this.cords.lat+","+this.cords.lng+"'>";
             return contentString;
         }
 
@@ -102,6 +72,7 @@ function initMap() {
             maxWidth: 200
         });
 
+        //Marker constructor
         var marker = new google.maps.Marker({
             position: new google.maps.LatLng(this.cords),
             title: this.title,
@@ -114,7 +85,7 @@ function initMap() {
 
         marker.addListener('click', function() {
             //Close all infoWindows and stop all animations
-            for (i in placeMarkers()){
+            for (var i in placeMarkers()){
                 if(placeMarkers()[i]().mapMarker.infoWindow) {
                     placeMarkers()[i]().mapMarker.infoWindow.close();
                     placeMarkers()[i]().mapMarker.setAnimation(null);
@@ -127,8 +98,8 @@ function initMap() {
             //Animate marker
             marker.setAnimation(google.maps.Animation.BOUNCE);
 
-            //Stop animating after 0.75 seconds
-            setTimeout(function(){marker.setAnimation(null);},750);
+            //Stop animating after 0.7 seconds
+            setTimeout(function(){marker.setAnimation(null);},700);
 
             //Open current infoWindow
             this.infoWindow.open(map, marker);
@@ -136,39 +107,108 @@ function initMap() {
         return marker;
     }
     //Push observable placeMarker object into the global array that is accessible by listView
-    for (i in places){
+    for (var i = 0; i < places.length; i++){
         var placeMarker = ko.observable({});
         placeMarker().mapMarker = MarkerInit(places[i]);
         placeMarker().visible = ko.observable(true);
         placeMarker().contentVisible = ko.observable(true);
         placeMarker().titleContent = placeMarker().mapMarker.titleContent;
         placeMarker().content = placeMarker().mapMarker.content;
+        placeMarker().wikiContent = ko.observable("");
 
         placeMarkers.push(placeMarker);
-    };
+    }
+
+    //Apply bindings after populating placeMarkers()[]
+    ko.applyBindings(new viewModel(), view);
+}
+
+//Handle error in case Google map fails to load
+function errorMap(){
+    alert("Google Maps failed to load. Please check your Internet connection");
 }
 
 //ViewModel
 var viewModel = function(){
 
-    self.listContentReveal = function(markerObj) {
-        //for (i in placeMarkers()){
-        //    if(placeMarkers()[i]().mapMarker.infoWindow) {
-        //       placeMarkers()[i]().mapMarker.infoWindow.close();
-        //    }
-        //}
+    //Variables for search and list view visiblities
+    self.searchVisible = ko.observable(true);
+    self.listVisible = ko.observable(true);
 
-        markerObj.contentVisible(!markerObj.contentVisible());
+    //Flip list visibility
+    self.listReveal = function(){
+        self.listVisible(!self.listVisible());
+    };
 
-        if (markerObj.contentVisible()) {
-            map.panTo(markerObj.mapMarker.position);
-            markerObj.mapMarker.infoWindow.open(map, markerObj.mapMarker);
-        }
-        else{
-            markerObj.mapMarker.infoWindow.close();
-        }
+    //Flip search visibility
+    self.searchReveal = function(){
+        self.searchVisible(!self.searchVisible());
+    };
+
+    function wikiInit(callback, index){
+        this.wikiMarkup = "";
+
+        var wikiTitle = places[i].title.replace(" ", "_");
+
+        //URL to search Wikipedia for articles from the title, and extract the introductory sections
+        var wikiURL = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles="+places[i].title;
+
+        //AJAX call to Wiki API
+        $.ajax({
+            type:"GET",
+            dataType:"jsonp",
+            url: wikiURL,
+
+            success: function(result){
+                callback(result.query.pages[Object.keys(result.query.pages)[0]].extract, index);
+            },
+            //Error handling in case Wiki articles don't load
+            error: function(){
+                callback("Failed to retrieve info");
+            }
+        });
     }
 
+    //Loop to make the AJAX call for each place
+    for (var i = 0; i < places.length; i++) {
+        //TODO: Carry function definition outside loop without breaking anything
+        wikiInit(function (wikiString, index) {
+
+            var wikiMarkup;
+
+            //Check if returned string is empty and instantiate the markup string to push
+            if (wikiString === "")
+            {
+                wikiMarkup = "<h4>Wikipedia:</h4>" + "<p>Could not find article</p>";
+            }
+            else {
+                wikiMarkup = "<h4>Wikipedia:</h4>" + "<p class='view-text'>" + wikiString + "</p>";
+            }
+
+            //Push wiki content too placeMarkers()[]
+            placeMarkers()[index]().wikiContent(wikiMarkup);
+        }, i);
+    }
+
+
+    self.listContentReveal = function(markerObj) {
+
+        //Flip the contentVisible property of each subsection on list
+        markerObj.contentVisible(!markerObj.contentVisible());
+
+        //Trigger the click even on the marker for the opened list item
+        if (markerObj.contentVisible()) {
+            //map.panTo(markerObj.mapMarker.position);
+            //markerObj.mapMarker.infoWindow.open(map, markerObj.mapMarker);
+            google.maps.event.trigger(markerObj.mapMarker, 'click');
+        }
+        else{
+            //Close the infoWindow for closed list item
+            markerObj.mapMarker.infoWindow.close();
+        }
+    };
+
+    //Initialize variables used by the filter
     this.phraseToFilter = ko.observable("");
 
     this.allCategories = ko.observableArray([]); //All filterable items
@@ -181,13 +221,23 @@ var viewModel = function(){
 
     this.categoriesToFilter = ko.observableArray(["Food"]);// Initial selection since everyone loves food
 
+    //Set visibility of all markers to true
+    this.unfilter = function(){
+        for (var i = 0; i<placeMarkers().length; i++) {
+            placeMarkers()[i]().visible(true);
+            placeMarkers()[i]().mapMarker.setVisible(true);
+        }
+    };
+
     this.filterByPhrase = function () {
         var markerVisibility;
-        for (i in placeMarkers()){
+        for (var i = 0; i<placeMarkers().length; i++) {
             markerVisibility = true;
-            if (placeMarkers()[i]().mapMarker.title.toLowerCase().match(this.phraseToFilter().toLowerCase()) == null){
+            //Match titles converted t lower case with search term converted to lower case. If match is null, set markerVisiblity to false
+            if (placeMarkers()[i]().mapMarker.title.toLowerCase().match(this.phraseToFilter().toLowerCase()) === null) {
                 markerVisibility = false;
-            };
+            }
+            //Set marker visibilities using markerVisibility
             placeMarkers()[i]().visible(markerVisibility);
             placeMarkers()[i]().mapMarker.setVisible(markerVisibility);
         }
@@ -195,9 +245,11 @@ var viewModel = function(){
 
     this.filterByCategories = function () {
         var markerVisibility;
-        for (i in placeMarkers()){
+
+        //Nested loop that goes over all selected categories and all places
+        for (var i = 0; i<placeMarkers().length; i++){
             markerVisibility = false;
-            for (j in this.categoriesToFilter()){
+            for (var j in this.categoriesToFilter()){ //Turning this into a regular for loop causes an error: placeMarkers()[i] is not a function
                 if (this.categoriesToFilter()[j] == placeMarkers()[i]().mapMarker.type){
                     markerVisibility = true;
                 }
@@ -205,10 +257,10 @@ var viewModel = function(){
                     continue;
                 }
             }
+            //Set marker visibilities
             placeMarkers()[i]().visible(markerVisibility);
             placeMarkers()[i]().mapMarker.setVisible(markerVisibility);
         }
-    }
+    };
 };
 
-ko.applyBindings(new viewModel(), view);
